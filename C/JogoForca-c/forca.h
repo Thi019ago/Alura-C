60a573204ed83b69784feb6f8fc2d898b5fac4c4
Thiago <thiagosoalb123@gmail.com>
2024-03-05 16:55:37 -0300

    Add files via upload

---
+++ C/JogoForca-c/forca.h
@@ -0,0 +1,10 @@
+#define TAMANHO_PALAVRA 20
+
+void chuta();
+void desenhaforca();
+void escolhepalavra();
+void adicionapalavra();
+int enforcou();
+int acertou();
+int chuteserrados();
+int jachutou(char letra);
